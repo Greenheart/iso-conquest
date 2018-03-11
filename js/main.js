c4c533ea92f0b4c75dc030d87ba2148e7c35a027
Samuel Plumppu <greenheartmusic@gmail.com>
2018-03-11 22:42:25 +0100

    feature(main menu): Add strategy tips.
    Also implemented a function to go back to the main menu. Will be reused
    for the game preparation ui.

--- js/main.js
+++ js/main.js
@@ -4,8 +4,16 @@
   const modal = document.querySelector('.modal')
   const mainMenu = document.querySelector('.main-menu')
   const playButton = mainMenu.querySelector('button.play')
+  const strategyButton = mainMenu.querySelector('button.strategy')
+  const menuContent = mainMenu.querySelector('.menu')
+  const strategyTips = mainMenu.querySelector('.strategy-tips')
 
   window.DEBUG = false
+
+  for (const b of mainMenu.querySelectorAll('button.back')) {
+    b.addEventListener('click', e => showMainMenu(e))
+  }
+  strategyButton.onclick = () => showStrategyTips()
   playButton.onclick = () => window.startNewGame()
 
   window.startNewGame = () => {
@@ -49,5 +57,16 @@
         }]
       })
     }
+  }
+
+  function showStrategyTips () {
+    Helpers.hideElement(menuContent)
+    Helpers.showElement(strategyTips)
+  }
+
+  function showMainMenu (event) {
+    const activeSection = mainMenu.querySelector(`.${event.target.dataset.section}`)
+    Helpers.hideElement(activeSection)
+    Helpers.showElement(menuContent)
   }
 })()
