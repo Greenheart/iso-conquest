0d8c89b3cfc4aa6baba905d818b286f106157eca
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2021-10-29 00:31:36 +0200

    Add initial UI

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,19 @@
-import preprocess from 'svelte-preprocess'
+import preprocess from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://github.com/sveltejs/svelte-preprocess
     // for more information about preprocessors
-    preprocess: preprocess(),
+    preprocess: [
+        preprocess({
+            postcss: true,
+        }),
+    ],
     // TODO: Add static adapter and build step
 
     kit: {
         // hydrate the <div id="svelte"> element in src/app.html
-        target: '#svelte',
+        target: "#svelte",
     },
 }
 
