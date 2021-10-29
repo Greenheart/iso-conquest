6ae7f7a82edcd0a6fcc5edd8cc7ef374d1b54f99
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2021-10-29 02:03:26 +0200

    basic game board

--- src/components/Game.svelte
+++ src/components/Game.svelte
@@ -4,9 +4,23 @@
 
 <script lang="ts">
     import { gameState } from "$lib/stores"
+    import Zone from "./Zone.svelte"
 
     export let map: Map
     $gameState = newGame(loadMap(map))
 </script>
 
-<pre>{JSON.stringify($gameState, null, 2)}</pre>
+<!-- TODO: render every zone -->
+<!-- TODO: clicking a zone should make it clear that it's selected -->
+<!-- TODO: show player scores -->
+<!-- TODO: Highlight the current player -->
+
+<div class="my-16" />
+
+<div
+    class="grid grid-cols-8 grid-rows-8 aspect-square max-w-6xl w-[90vw] mx-auto select-none"
+>
+    {#each $gameState.zones as zone}
+        <Zone {zone} />
+    {/each}
+</div>
---
+++ src/components/Zone.svelte
@@ -0,0 +1,16 @@
+<script lang="ts" context="module">
+    import type { Zone } from "$game/game"
+</script>
+
+<script lang="ts">
+    import { gameState } from "$lib/stores"
+
+    export let zone: Zone
+</script>
+
+<div
+    class:shadow-xl={zone.type !== "default"}
+    class="grid place-items-center bg-teal-900 border-2 border-transparent hover:border-emerald-500"
+>
+    {zone.owner ? zone.owner.id : ""}
+</div>
