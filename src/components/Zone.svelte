d2a0ace4d14cdbcd8b209a9181da1809aafd9e3b
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2021-10-30 13:18:27 +0200

    Improve game logic

--- src/components/Zone.svelte
+++ src/components/Zone.svelte
@@ -26,6 +26,9 @@
         $selectedZone !== zone &&
         $conquerableBySacrifice.some((z) => isSame(z, zone))
 
+    // IDEA: Maybe cache all adjacent zones for each zone to make runtime checks faster
+    // Will especially be useful when adding minimax AI
+
     function getBgColor() {
         if (zone.owner && $playerColors) return $playerColors[zone.owner.id]
         if (isConquerable) return "bg-teal-500"
@@ -42,43 +45,46 @@
     function handleClick() {
         if (isOwnZone) {
             $selectedZone = zone
-            $conquerable = $getAdjacentZones(zone, 1)
-            $conquerableBySacrifice = $getAdjacentZones(zone, 2)
+            $conquerable = $getAdjacentZones(zone, 1).filter((z) => !z.owner)
+            $conquerableBySacrifice = $getAdjacentZones(zone, 2).filter(
+                (z) => !z.owner,
+            )
             return
         }
 
-        if (isConquerable) {
-            $gameState = conquer($gameState, {
-                player: $gameState.currentPlayer,
-                origin: $selectedZone as Zone,
-                target: zone,
-            })
-        } else if (isConquerableBySacrifice) {
-            $gameState = conquerBySacrifice($gameState, {
-                player: $gameState.currentPlayer,
-                origin: $selectedZone as Zone,
-                target: zone,
-            })
+        const action = {
+            player: $gameState.currentPlayer,
+            origin: $selectedZone as Zone,
+            target: zone,
         }
 
-        reset()
+        if (isConquerable) {
+            $gameState = conquer($gameState, action)
+        } else if (isConquerableBySacrifice) {
+            $gameState = conquerBySacrifice($gameState, action)
+        }
+
+        if ($selectedZone) reset()
     }
 </script>
 
-<!-- TODO: make only hoverable when you can select the zone, otherwise let the zone be -->
-<!-- See original game for UI ideas -->
-
 <div
-    class:shadow-xl={zone.type !== "default"}
-    class={"grid place-items-center border-2 border-transparent" +
+    class={"grid place-items-center border-2 border-transparent relative" +
         ` ${getBgColor()}` +
         `${
             isConquerable ||
             isConquerableBySacrifice ||
             isOwnZone ||
+            // TODO: and it should have conquerable neighbors
             $selectedZone === zone
                 ? " hover:border-white"
                 : ""
         }`}
     on:click={handleClick}
-/>
+>
+    <p
+        class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+    >
+        {zone.type !== "default" ? zone.value : ""}
+    </p>
+</div>
